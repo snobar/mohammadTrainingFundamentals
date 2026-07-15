4b1212fd3d55efefe68b242723e292a8bd4962f2
Mohammad Makhoul <mmakhoul274@gmail.com>
2026-07-15 09:12:20 +0300

    Multiplication table

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.WriteLine("\t\t\t\tMultiplication_1_TO_10");
+
+for (int P_Head = 1; P_Head <= 10; ++P_Head)
+{
+    Console.Write("\t" + P_Head);
+}
+
+Console.WriteLine("\n______________________________________________________________________________________\n");
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i < 10)
+        Console.Write(" " + i + "   |");
+    else
+        Console.Write(i + "  |");
+
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.Write("\t" + (i * j));
+    }
+
+    Console.WriteLine();
+}
+
